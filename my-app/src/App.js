315318c772d9de571f69d74d4b8ffdfc542cd963
MiyamuP <pazu119@icloud.com>
2023-11-19 00:21:30 +0900

    記事一覧と、記事画面のプロトタイプ

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Article from './pages/Article'
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/article' element={<Article />} />
--- my-app/src/pages/Article.js
+++ my-app/src/pages/Article.js
@@ -1,7 +1,28 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useLocation } from 'react-router-dom';
+
 export const Article = () => {
+    const location = useLocation();
+    console.log(location);
     return (
-        <div>Article</div>
+        <div class="ml-10">
+            <br />
+            <div class="container py-4" id="works">
+                <div class="d-flex justify-content-center">
+                    <div class="border-bottom border-warning">
+                        <h2>{location.state.article.Article_title}</h2>
+                    </div>
+                </div>
+                <div class="text-right">
+                    <br /><p class="mr-5">制作日時: {location.state.article.create_time}</p><p class="mr-5">作成者名: {location.state.article.Author}</p><br />
+                </div>
+                <div class="mx-5 border rounded border-warning">
+                    <br />
+                    <p class="mx-5 rounded-text">メッセージ内容<br />{location.state.article.text}</p>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Article;
