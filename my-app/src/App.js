7bb66f767757c48eb3fc30ac40fb59ee5090d195
MiyamuP <pazu119@icloud.com>
2023-11-19 10:30:38 +0900

    Eventの修正

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Home from './pages/Event'
+import Home from './pages/Home'
 import Article from './pages/Article'
 import Event from './pages/Event'
 import Header from './components/Header';
