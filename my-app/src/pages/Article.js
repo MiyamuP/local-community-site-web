7098351649d401f3c4241ec7f0b2dfd30a1c7eae
MiyamuP <pazu119@icloud.com>
2023-11-19 11:13:12 +0900

    コメントの側を追加

--- my-app/src/pages/Article.js
+++ my-app/src/pages/Article.js
@@ -22,6 +22,39 @@ export const Article = () => {
                     <p className="mx-5 rounded-text">メッセージ内容<br />{location.state.article.text}</p>
                 </div>
             </div>
+
+            <div class="mx-auto p-2" style={{ width: "650px;" }}>
+                <div class="p-3 mb-2 bg-warning">
+                    <div class="d-flex justify-content-center">
+                        <h3>Comment</h3>
+                    </div>
+                    <div class="mx-auto p-2" style={{ width: "600px;" }}>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">ユーザー名1</h5>
+                                <p class="card-text"><small class="text-muted ml-2">投稿日</small></p>
+                                <p class="card-text">コメント内容</p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">ユーザー名2</h5>
+                                <p class="card-text"><small class="text-muted ml-2">投稿日</small></p>
+                                <p class="card-text">コメント内容</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="p-3 mb-2 mt-5 bg-white">
+                        <form class="m-2">
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <div class="d-flex justify-content-center mt-2">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
