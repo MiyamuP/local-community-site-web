5461c4915e17d8e9b92f26008d013838af1abb57
MiyamuP <pazu119@icloud.com>
2023-11-19 09:29:38 +0900

    Routeの追加

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Event'
 import Article from './pages/Article'
+import Event from './pages/Event'
 import Header from './components/Header';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route path='/event/:id' element={<Event />} />
         <Route path='/article' element={<Article />} />
       </Routes>
     </BrowserRouter>
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,63 +1,7 @@
 import React from "react";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from "react-router";
-
-
 export const Home = () => {
-    const navigate = useNavigate()
-    let articles = [
-        {
-            Article_id: 1,
-            Article_title: "記事１",
-            Author: "宮村一希",
-            text: "記事の本文１だよ！！",
-            create_time: "2023-11-17-10-30",
-            Comment_id: 1,
-            Prefecture_id: 1,
-        }, {
-            Article_id: 2,
-            Article_title: "記事２",
-            Author: "宮村一希",
-            text: "記事の本文２だよ！！",
-            create_time: "2023-11-17-10-30",
-            Comment_id: 2,
-            Prefecture_id: 2,
-        }
-    ];
-
-    const toArticle = (val) => {
-        console.log(val);
-        navigate('/article', { state: { article: val } });
-    }
-
     return (
-        <div className="container py-4" id="works">
-            <div className="d-flex justify-content-center">
-                <h2>地域コミュニティサイト</h2>
-            </div>
-            <div className="d-flex justify-content-center">
-                <p>このサイトは、地域内外の交流を目的としたサイトです。</p><br /><br /><br />
-            </div>
-            <div className="row row-cols-3 row-cols-md-3 g-4">
-                {articles.map((val) => (
-
-                    <div className="col" key={val.Article_id}>
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{val.Article_title}</h5>
-                                <p className="card-text">{val.Author}</p>
-                                <p className="card-text"><small className="text-muted">{val.create_time}</small></p>
-                                <div style={{ textAlign: "right" }}><button className="btn btn-primary" onClick={() => toArticle(val)}>もっと見る</button></div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-
-
-            </div>
-        </div>
+        <div>Home</div>
     );
 }
 export default Home;
